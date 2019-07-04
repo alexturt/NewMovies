ce48b17faea673f044dda2a68eb67b650dc99ec2
Kate <Bondarenkokate73@yandex.ru>
2019-07-04 12:48:46 +0400

    Fix pars

--- projectE/MainWindow.xaml.cs
+++ projectE/MainWindow.xaml.cs
@@ -41,14 +41,9 @@ namespace projectE
             { }
         }
 
-        int limitWind;
-        [STAThread]
         public void ShowBox(string v)
         {
-            thread = new Thread(Upd);
-            thread.Abort();
             MessageBox.Show(v, "Ошибка", MessageBoxButton.OK);
-            isRun = false;
         }
 
         DB db = new DB();
@@ -245,20 +240,23 @@ namespace projectE
             }
 
             TextBlock tb = new TextBlock();
-            tb.Inlines.Add(createURL(dt_movies.Rows[index]["URLinfo"].ToString(), dt_movies.Rows[index]["name"].ToString(), 22, Brushes.AliceBlue));
-            Run run = new Run()
+            try
             {
-                FontSize = 16,
-                Text = Environment.NewLine +
-                    dt_movies.Rows[index]["year"].ToString() + "\r\n\r\n" +
-                    dt_movies.Rows[index]["country"].ToString() + "\r\n\r\n" +
-                    dt_movies.Rows[index]["genres"].ToString() + "\r\n\r\n" +
-                    ConvertDate(dt_movies.Rows[index]["date"].ToString()) + "\r\n\r\n" +
-                    dt_movies.Rows[index]["agerating"].ToString() + "\r\n\r\n",
-                Foreground = Brushes.LightGray,
-                Tag = index
-            };
-            tb.Inlines.Add(run);
+                tb.Inlines.Add(createURL(dt_movies.Rows[index]["URLinfo"].ToString(), dt_movies.Rows[index]["name"].ToString(), 22, Brushes.AliceBlue));
+                Run run = new Run()
+                {
+                    FontSize = 16,
+                    Text = Environment.NewLine +
+                        dt_movies.Rows[index]["year"].ToString() + "\r\n\r\n" +
+                        dt_movies.Rows[index]["country"].ToString() + "\r\n\r\n" +
+                        dt_movies.Rows[index]["genres"].ToString() + "\r\n\r\n" +
+                        ConvertDate(dt_movies.Rows[index]["date"].ToString()) + "\r\n\r\n" +
+                        dt_movies.Rows[index]["agerating"].ToString() + "\r\n\r\n",
+                    Foreground = Brushes.LightGray,
+                    Tag = index
+                };
+                tb.Inlines.Add(run);
+
 
             if (dt_movies.Rows[index]["URLtrailer"].ToString() != "")
                 tb.Inlines.Add(createURL(dt_movies.Rows[index]["URLtrailer"].ToString(), "Трейлер\r\n", 14, Brushes.AliceBlue));
@@ -288,6 +286,8 @@ namespace projectE
             Grid.SetColumnSpan(tb2, 2);
             Grid.SetRow(tb2, 1);
             grid_right.Children.Add(tb2);
+            }
+            catch { }
         }
         //для вкладки подробнее создание гиперссылок
         private Hyperlink createURL(string url, string name, int size, Brush brush)
